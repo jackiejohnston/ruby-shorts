b74450e2580d5419d4e353f98b77a8486d33fe71
Jackie Johnston <jackiejohnston@users.noreply.github.com>
2013-08-18 08:23:50 -0700

    Write a method, pow, that takes two (non-negative, integer) numbers, base and exponent and returns base raised to the exponent power. (No fair using Ruby'sbase ** exponent notation!).

--- lib/exponential_no_shortcut.rb
+++ lib/exponential_no_shortcut.rb
@@ -1,6 +1,6 @@
-# Write a method, pow, that takes two (non-negative, integer) numbers, base 
-# and exponent and returns base raised to the exponent power. (No fair using 
-# Ruby's base ** exponent notation!).
+#  Write a method, pow, that takes two (non-negative, integer) numbers, base 
+#  and exponent and returns base raised to the exponent power. (No fair using 
+#  Ruby's base ** exponent notation!).
 
 def pow (base, exponent) 
   result = 1
