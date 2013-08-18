02dbaf28b9cdb6abde4691e374a1a155ff8806ad
Jackie Johnston <jackiejohnston@users.noreply.github.com>
2013-08-18 11:07:35 -0700

    Determine if a sentence is a palindrome

---
+++ lib/primer_143_palindrome.rb
@@ -0,0 +1,18 @@
+=begin
+
+Problem Statement
+
+Given a sentence, return true if the sentence is a palindrome.
+
+You are supposed to write a method palindrome? to accomplish this task.
+
+Note Ignore whitespace and cases of characters.
+
+Example:
+Given "Never odd or even" the method should return true
+
+=end
+
+def palindrome?(sentence)
+  sentence.downcase.gsub(/\s+/, "") == sentence.downcase.reverse.gsub(/\s+/, "")
+end
