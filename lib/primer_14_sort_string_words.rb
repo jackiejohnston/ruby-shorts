b98269aa19858a100e5b94190c0cefff3229a88b
Jackie Johnston <jackiejohnston@users.noreply.github.com>
2013-08-18 10:54:43 -0700

    Sort the words in a given sentence by length

--- lib/primer_14_sort_string_words.rb
+++ lib/primer_14_sort_string_words.rb
@@ -1,7 +1,6 @@
 =begin
 
 Problem Statement
-
 Create a method named 'sort_string' which accepts a String and rearranges all 
 the words in ascending order, by length. Let's not treat the punctuation marks 
 any different than other characters and assume that we will always have single 
