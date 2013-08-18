d5c481bd173c458ec477c01b16253e92cfed8e9e
Jackie Johnston <jackiejohnston@users.noreply.github.com>
2013-08-18 08:14:01 -0700

    Update pow.rb

--- lib/pow.rb
+++ lib/pow.rb
@@ -1,5 +1,9 @@
-def pow (base, integer) 
-    result = base
-    (integer-1).times {result = result * base}
-    return result
+# Write a method, pow, that takes two (non-negative, integer) numbers, base 
+# and exponent and returns base raised to the exponent power. (No fair using 
+# Ruby's base ** exponent notation!).
+
+def pow (base, exponent) 
+  result = 1
+  exponent.times {result = result * base}
+  return result
 end
